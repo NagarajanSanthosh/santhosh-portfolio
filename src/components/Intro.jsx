a04d63424f67919fd71e26359f05b29bac355877
NagarajanSanthosh <64208ns@gmail.com>
2024-05-14 23:23:49 +0530

    profile image

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,15 +3,16 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import image from '../image/profile2.jpg'
 import { Container, Row, Col } from 'react-bootstrap'
 import '../index.css'
+
 const Intro = () => {
     const openPDF = () => {
         window.open('https://drive.google.com/file/d/1SiwTAbO2DALuv1Sj7HuWFfpfXUf625Wo/view?usp=sharing', '_blank');
     }
+
     return (
         <div className='intro-section' style={{ minHeight: '100vh', display: 'flex', alignItems: 'center' }}>
             <Container>
                 <Row className='gx-4'>
-
                     <Col md={6}>
                         <div className='d-flex justify-content-center align-items-center'>
                             <img className='image-style' src={image} alt='profile' />
@@ -19,16 +20,14 @@ const Intro = () => {
                     </Col>
                     <Col md={6}>
                         <div className='d-flex flex-column justify-content-center'>
-                            <p className=''>Hi, I'm</p>
+                            <p>Hi, I'm</p>
                             <p className='fw-bold fs-3 name'>Santhosh</p>
                             <p>Frontend Developer</p>
                             <button className='btn btn-dark' onClick={openPDF}>Resume</button>
                         </div>
-
                     </Col>
                 </Row>
             </Container>
-
         </div>
     )
 }
