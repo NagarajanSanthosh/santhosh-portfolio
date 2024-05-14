1c184e7a3e87183d4909c002c151e9777b9cd4a7
NagarajanSanthosh <64208ns@gmail.com>
2024-05-14 23:11:31 +0530

    modified contact look and icons inserted

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import '../index.css';
+import '../contact.css';
+import { MdEmail } from 'react-icons/md';
+import { FaLinkedin } from 'react-icons/fa';
 
 const Contact = () => {
   return (
     <div id='contact' className='bottom-style'>
       <Container>
-
-
         <Row className='justify-content-center'>
           <Col xs={12} className='text-center mb-4'>
             <p className='intro-text'>Get in Touch</p>
@@ -15,22 +15,13 @@ const Contact = () => {
           </Col>
         </Row>
         <Row>
-          <footer className="bg-dark text-white p-4">
-            <div className="container">
-              <div className="row">
-                <div className="col-lg-12 text-center">
-                  <p>Email: <a href='mailto:santhoshn2982@gmail.com'>santhoshn2982@gmail.com</a></p>
-                  <p>LinkedIn: <a href="https://www.linkedin.com/in/santhosh-nagarajan-98686923b/">LinkedIn/Santhosh</a></p>
-                </div>
-              </div>
-              <div className='row'>
-                <div className="col-lg-12 text-center">
-                  <p>Copyrights © reserved 2024</p>
-                </div>
-
-              </div>
-            </div>
-          </footer>
+          <Col xs={12}>
+            <footer className='footer-content'>
+              <a className="contact-a" href='mailto:santhoshn2982@gmail.com'><MdEmail size={30} /></a>
+              <a className="contact-a" href="https://www.linkedin.com/in/santhosh-nagarajan-98686923b/" target="_blank" rel="noopener noreferrer"><FaLinkedin size={30} /></a>
+              <p>© 2024 All Rights Reserved</p>
+            </footer>
+          </Col>
         </Row>
       </Container>
     </div>
